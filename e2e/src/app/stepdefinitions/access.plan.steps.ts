f68d71ff42e20b57c482414ddd2facd431416f3a
Blake <blake.percy@resonate.tech>
2020-09-21 16:51:10 +0100

    Example access-plan test added

---
+++ e2e/src/app/stepdefinitions/access.plan.steps.ts
@@ -0,0 +1,19 @@
+import {Before, When} from 'cucumber';
+import {LinxRestClient} from '../api/linx/linx-rest-client';
+import {AppPage} from "../pages/app.po";
+import * as fs from 'fs';
+import {AccessPlanRequest} from "../../../../src/app/api/linx/models/access-plan-request";
+
+let page: AppPage;
+let linxRestClient: LinxRestClient;
+
+Before(() => {
+  page = new AppPage();
+  linxRestClient = new LinxRestClient();
+});
+
+When('the access plan located in JSON file {string} is received from LINX', async (jsonFilePath: string) => {
+  let rawData: Buffer = fs.readFileSync(jsonFilePath);
+  let accessPlanRequest: AccessPlanRequest = JSON.parse(rawData.toString());
+  linxRestClient.writeAccessPlan(accessPlanRequest);
+});
