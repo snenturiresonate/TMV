612d4bb8cce243fb0ab4a766502f828e8dbdfedf
Venkataraman <225138@cognizant.com>
2021-02-09 18:47:00 +0000

    Default navigation implemented to be admin role

--- e2e/src/app/pages/app.po.ts
+++ e2e/src/app/pages/app.po.ts
@@ -1,16 +1,53 @@
 import {browser, by, element, ElementArrayFinder, ElementFinder } from 'protractor';
+import {AuthenticationModalDialoguePage} from './authentication-modal-dialogue.page';
+import {CommonActions} from './common/ui-event-handlers/actionsAndWaits';
 
 export class AppPage {
   public modalWindow: ElementFinder;
   public modalWindowButtons: ElementArrayFinder;
-
-  public navigateTo(url: string): Promise<unknown> {
-    return browser.get(browser.baseUrl + url) as Promise<unknown>;
-  }
+  public navBarLogo: ElementFinder;
 
   constructor() {
     this.modalWindow = element(by.css('.modalpopup'));
     this.modalWindowButtons = element.all(by.css('.modalpopup .tmv-btn'));
+    this.navBarLogo = element(by.css('.navbar-brand [alt=logo]'));
+  }
+
+  public async navigateTo(url: string, role?: string): Promise<any> {
+    if (role) {
+      return browser.get(browser.baseUrl + url)
+        // Click Login button (if displayed) step to be added
+        .then(() => this.authenticationRouter(role))
+        .then(() => browser.waitForAngularEnabled(true))
+        .then(() => CommonActions.waitForElementToBeVisible(this.navBarLogo));
+    } else {
+      return browser.get(browser.baseUrl + url)
+        // Click Login button (if displayed) step to be added
+        .then(() => this.authenticateAsAdminUser())
+        .then(() => browser.waitForAngularEnabled(true))
+        .then(() => CommonActions.waitForElementToBeVisible(this.navBarLogo));
+    }
+  }
+
+  private async authenticationRouter(role: string): Promise<any> {
+    let authentication;
+    switch (role.toLowerCase()) {
+      case('admin'):
+        authentication = this.authenticateAsAdminUser();
+        break;
+      case('restricted'):
+        authentication = this.authenticateAsRestrictedUser();
+        break;
+      case('standard'):
+        authentication = this.authenticateAsStandardUser();
+        break;
+      case('schedulematching'):
+        authentication = this.authenticateAsScheduleMatchingUser();
+        break;
+      default:
+        throw new Error('Please verify the authentication role used');
+    }
+    return authentication;
   }
 
   public async getModalWindowTitle(): Promise<string> {
@@ -20,6 +57,36 @@ export class AppPage {
 
   public async getModalButtons(): Promise<string> {
     return this.modalWindowButtons.getText();
+  }
+
+  public async authenticateAsAdminUser(): Promise<void> {
+    const userName = 'userAdmin';
+    const Password = 'password';
+    await this.performLoginSteps(userName, Password);
+  }
+
+  public async authenticateAsStandardUser(): Promise<void> {
+    const userName = 'userStandard';
+    const Password = 'password';
+    await this.performLoginSteps(userName, Password);
+  }
+
+  public async authenticateAsRestrictedUser(): Promise<void> {
+    const userName = 'userRestrictions';
+    const Password = 'password';
+    await this.performLoginSteps(userName, Password);
+  }
+
+  public async authenticateAsScheduleMatchingUser(): Promise<void> {
+    const userName = 'userScheduleMatching';
+    const Password = 'password';
+    await this.performLoginSteps(userName, Password);
+  }
+
+  public async performLoginSteps(userName: string, Password: string): Promise<void> {
+    const authPage: AuthenticationModalDialoguePage = new AuthenticationModalDialoguePage();
+    await browser.waitForAngularEnabled(false);
+    await authPage.authenticate(userName, Password);
   }
 
 }
