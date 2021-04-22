b4db31efab04ef42a03f4476b57f64bf0a1b3712
Clare Fuller <Clare.Fuller@resonate.tech>
2021-04-22 16:54:00 +0000

    Merged PR 19754: Remove tags for 59973
    Remove tags for 59973
    
    Related work items: #59973

--- e2e/src/app/pages/trains-list-config/trains.list.config.common.page.ts
+++ e2e/src/app/pages/trains-list-config/trains.list.config.common.page.ts
@@ -1,12 +1,13 @@
 import {by, element, ElementArrayFinder, ElementFinder} from 'protractor';
-import {CommonActions} from '../common/ui-event-handlers/actionsAndWaits';
 
 export class TrainsListConfigCommonPage {
   public trainListConfigSaveBtn: ElementArrayFinder;
   public trainListConfigResetBtn: ElementArrayFinder;
+  public unsavedIndicators: ElementArrayFinder;
   constructor() {
     this.trainListConfigSaveBtn = element.all(by.cssContainingText('span', 'Save'));
     this.trainListConfigResetBtn = element.all(by.cssContainingText('Span', 'Reset'));
+    this.unsavedIndicators = element.all(by.css('.unsaved'));
   }
 
   public async saveTrainListConfig(): Promise<void> {
@@ -15,6 +16,9 @@ export class TrainsListConfigCommonPage {
 
   public async resetTrainListConfig(): Promise<void> {
     return this.findVisibleBtn(this.trainListConfigResetBtn).click();
+  }
+  public async isUnsaved(): Promise<boolean> {
+    return (await this.unsavedIndicators.count() > 0);
   }
 
   public findVisibleBtn(btnLocator: ElementArrayFinder): ElementFinder {
--- e2e/src/app/stepdefinitions/trains-list-config/trains-list-config-common.steps.ts
+++ e2e/src/app/stepdefinitions/trains-list-config/trains-list-config-common.steps.ts
@@ -1,10 +1,19 @@
 import {TrainsListConfigCommonPage} from '../../pages/trains-list-config/trains.list.config.common.page';
-import {When} from 'cucumber';
+import {When, Then} from 'cucumber';
+import { expect } from 'chai';
 
 const page: TrainsListConfigCommonPage = new TrainsListConfigCommonPage();
 
 When('I save the trains list config', async () => {
   await page.saveTrainListConfig();
+});
+
+Then('There is an unsaved indicator', async () => {
+  expect(await page.isUnsaved()).to.equal(true);
+});
+
+Then('There is no unsaved indicator', async () => {
+  expect(await page.isUnsaved()).to.equal(false);
 });
 
 When('I reset the trains list config', async () => {
